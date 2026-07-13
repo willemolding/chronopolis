8207f9d092c9bd1b1b36998689e368f2c660603a
Willem Olding <willemolding@gmail.com>
2026-07-13 12:23:53 +1000

    make reusable basic case

--- src/faces/basic.rs
+++ src/faces/basic.rs
@@ -3,25 +3,29 @@ use crate::clock_hand::{ClockHand, Easing};
 use nannou::prelude::*;
 
 #[derive(Debug)]
-pub struct SistersFace {
+pub struct BasicFace {
     hour_hand: ClockHand,
     min_hand: ClockHand,
     sec_hand: ClockHand,
+    name: &'static str,
+    path_prefix: &'static str,
 }
 
-impl SistersFace {
-    pub fn new() -> Self {
+impl BasicFace {
+    pub fn new(name: &'static str, path_prefix: &'static str) -> Self {
         Self {
             hour_hand: ClockHand::new().easing(Easing::Linear).duration(60. * 60.),
             min_hand: ClockHand::new().easing(Easing::Linear).duration(60.),
             sec_hand: ClockHand::sweeping(),
+            name,
+            path_prefix,
         }
     }
 }
 
-impl ClockFace for SistersFace {
+impl ClockFace for BasicFace {
     fn name(&self) -> &str {
-        "Sisters"
+        self.name
     }
 
     fn update(&mut self, _app: &App, ctx: &crate::ClockContext) {
@@ -34,21 +38,21 @@ impl ClockFace for SistersFace {
         let size = f32::min(app.window_rect().w(), app.window_rect().h());
         let wh = vec2(size, size);
 
-        draw.texture(ctx.texture("sisters/bg"))
+        draw.texture(ctx.texture(&format!("{}/bg", self.path_prefix)))
             .xy(app.window_rect().xy())
             .wh(wh);
 
-        draw.texture(ctx.texture("sisters/mins"))
-            .xy(app.window_rect().xy())
-            .wh(wh)
-            .rotate(-self.min_hand.angle());
-
-        draw.texture(ctx.texture("sisters/hours"))
+        draw.texture(ctx.texture(&format!("{}/hours", self.path_prefix)))
             .xy(app.window_rect().xy())
             .wh(wh)
             .rotate(-self.hour_hand.angle());
 
-        draw.texture(ctx.texture("sisters/secs"))
+        draw.texture(ctx.texture(&format!("{}/mins", self.path_prefix)))
+            .xy(app.window_rect().xy())
+            .wh(wh)
+            .rotate(-self.min_hand.angle());
+
+        draw.texture(ctx.texture(&format!("{}/secs", self.path_prefix)))
             .xy(app.window_rect().xy())
             .wh(wh)
             .rotate(-self.sec_hand.angle());
--- src/faces/mod.rs
+++ src/faces/mod.rs
@@ -1,23 +1,17 @@
 use crate::clock_face::ClockFace;
+use basic::BasicFace;
 
 pub mod analog;
-pub mod burndial;
+pub mod basic;
 pub mod clocktown;
-pub mod kelpforest;
-pub mod mandala;
-pub mod purpcryst;
-pub mod scrimbles;
-pub mod sisters;
 
 pub fn all() -> Vec<Box<dyn ClockFace>> {
     vec![
         Box::new(analog::AnalogFace::new()),
-        Box::new(scrimbles::ScrimblesFace::new()),
         Box::new(clocktown::ClocktownFace::new()),
-        Box::new(burndial::BurndialFace::new()),
-        Box::new(purpcryst::PurpcrystFace::new()),
-        Box::new(kelpforest::KelpforestFace::new()),
-        Box::new(mandala::MandalaFace::new()),
-        Box::new(sisters::SistersFace::new()),
+        Box::new(BasicFace::new("Burndial", "burndial")),
+        Box::new(BasicFace::new("Kelp Forest", "kelpforest")),
+        Box::new(BasicFace::new("Sisters", "sisters")),
+        Box::new(BasicFace::new("Scrimbles", "scrimbles")),
     ]
 }
